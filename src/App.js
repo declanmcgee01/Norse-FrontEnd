9883f97e9ff6c948b10457456f2186378b4d6ab4
declanmcgee01 <declanmcgee1995@gmail.com>
2019-01-16 17:22:46 +0000

    A few additions made, such as background and header

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './NavBar';
+import SearchBar from './SearchBar';
 
 class App extends Component {
   render() {
@@ -10,7 +11,15 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <NavBar/>
+          <h1 id = "Home-Page-Header">
+            Norse Gods 
+          </h1> 
+          <body>
+            <input type="text" placeholder="Search Gods.." name="search" id = "Search-Bar"></input>
+          </body>
         </header>
+        <body>
+        </body>
       </div>
     );
   }
